cb8a45f8f62ef47abf3b1c2660918def58472a53
Flippi91 <g.delphis@outlook.com>
2021-09-29 20:11:16 +0200

    changed bg color and font color

--- Kalender-Widget-First-One/kalender-witget.php
+++ Kalender-Widget-First-One/kalender-witget.php
@@ -6,7 +6,7 @@
 	// Tag Der Woche ------------------
 	$weekday = strftime('%A');
 
-	// Tag Monat und Jahr --------------
+	// Tag Monat und Jahr
 	$day = strftime('%d');
 	$month = strftime('%B');
 	$year = strftime('%Y');
@@ -20,16 +20,17 @@
 	<title>KALENDER WIDGET</title>
 	<meta charset="UTF-8">
 </head>
-<body>
+<body style="background:#222; display: flex; justify-content:center; align-items:center; 
+flex-direction: column;">
 
-<h3 style="color:#999999;">KALENDER WIDGET</h3>
+<h3 style="color:#fff;">KALENDER WIDGET</h3>
 
-<div style="border:1px solid black;border-top:5px solid #000000; width:200px; height:250px;text-align:center;">
+<div style="border:1px solid white;border-top:5px solid #fff; color:#fff; width:200px; height:250px;text-align:center;">
 	<h2><?php echo $weekday; ?></h2>
 
 	
-	<span style="font-size:100px;font-weight:bold;"><?php echo $day;?></span>
-	<h2><?php echo $month.' '.$year; ?></h2>
+	<span style="font-size:100px;font-weight:bold; color:#fff;"><?php echo $day;?></span>
+	<h2 style="color:#fff;"><?php echo $month.' '.$year; ?></h2>
 </div>
 
 </body>
